c056e31636ed7c408b1221da25215397ee138556
changjones1 <37631460+changjones1@users.noreply.github.com>
2023-02-14 09:02:59 -0700

    i think github pages is just slow

--- script/main.js
+++ script/main.js
@@ -1,13 +1,14 @@
 let Phrase = require("changjones1-palindrome");
+alert("alert not in a funciton");
 function jsAlert() {
 	alert("JavaScript is working.");
 }
 function jsAlertTime() {
 	let now = new Date();
-//	alert(`JS is working ${now}`);
-
-//	alert(new Phrase("Madam, I'm Adam.").palindrome());
-//	alert("fuck");
+	alert(`JS is working ${now}`);
+	alert("alerts in a function");
+	alert(new Phrase("Madam, I'm Adam.").palindrome());
+	alert("fuck");
 	alert("fuck");
 }
 addEventListener('DOMContentLoaded', (event) => {
