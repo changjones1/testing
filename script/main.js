870ec4e5d0d61ee112de0aa64e97361722a02221
changjones1 <37631460+changjones1@users.noreply.github.com>
2023-02-14 09:20:01 -0700

    idefk githubpages is slow or something

--- script/main.js
+++ script/main.js
@@ -1,15 +1,15 @@
-//let Phrase = require("changjones1-palindrome");
-//alert("alert not in a funciton");
+let Phrase = require("changjones1-palindrome");
+alert("alert not in a funciton");
+alert(new Phrase("Madam, I'm Adam.").palindrome());
 function jsAlert() {
 	alert("JavaScript is working.");
 }
 function jsAlertTime() {
 	let now = new Date();
-//	alert(`JS is working ${now}`);
+	alert(`JS is working ${now}`);
 //	alert("alerts in a function");
-//	alert(new Phrase("Madam, I'm Adam.").palindrome());
 //	alert("fuck");
-	alert("fuck");
+//	alert("fuck");
 }
 addEventListener('DOMContentLoaded', (event) => {
 	jsAlertTime();
