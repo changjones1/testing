8c0433395c73026364960bf35a2c5b4e910eb827
changjones1 <37631460+changjones1@users.noreply.github.com>
2023-02-14 08:59:00 -0700

    moved alerts into function

--- script/main.js
+++ script/main.js
@@ -1,12 +1,14 @@
 let Phrase = require("changjones1-palindrome");
-alert(new Phrase("Madam, I'm Adam.").palindrome());
-alert("fuck");
 function jsAlert() {
 	alert("JavaScript is working.");
 }
 function jsAlertTime() {
 	let now = new Date();
 	alert(`JS is working ${now}`);
+
+	alert(new Phrase("Madam, I'm Adam.").palindrome());
+	alert("fuck");
+	alert("fuck");
 }
 addEventListener('DOMContentLoaded', (event) => {
 	jsAlertTime();
